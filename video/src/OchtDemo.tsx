c7d7ab5e3ffd9f730be863a367facb7c68ee8409
JohnTurbitt <johnturbitt@hotmail.com>
2026-06-13 13:12:41 +0100

    Fix JSX.Element type error in OchtDemo scene sequence

--- video/src/OchtDemo.tsx
+++ video/src/OchtDemo.tsx
@@ -1,3 +1,4 @@
+import type { JSX } from "react";
 import { AbsoluteFill } from "remotion";
 import { TransitionSeries, linearTiming } from "@remotion/transitions";
 import { fade } from "@remotion/transitions/fade";
